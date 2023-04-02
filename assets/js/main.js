e199d0c5d015e06fdbdbdb75f3238d42aa9c1afc
khilda <joinyou42995@naver.com>
2023-04-03 03:28:30 +0900

    서브메인디자인 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,17 @@
-const swiper = new Swiper("#cardSwiper", {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 27,
-  pagination: {
-    enabled: true,
-    el: ".swiper-pagination",
-    type: "bullets",
-  },
-  breakpoints: {
-    742: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1440: {
-      slidesPerView: 4,
-      spaceBetween: 36,
-      pagination: false,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    },
-  },
-});
 // main page
-document.querySelector("#gnbOpen").addEventListener("click", () => {
+document.querySelector("#gnbOpen")?.addEventListener("click", () => {
   document.querySelector(".header-menu").classList.add("is-show");
   document.body.setAttribute("style", "overflow: hidden");
 });
-document.querySelector("#gnbClose").addEventListener("click", () => {
+document.querySelector("#gnbClose")?.addEventListener("click", () => {
   document.querySelector(".header-menu").classList.remove("is-show");
   document.body.removeAttribute("style");
 });
-document.querySelector("#toggleTheme").addEventListener("click", (e) => {
+document.querySelector("#toggleTheme")?.addEventListener("click", (e) => {
   e.target.classList.toggle("toggle");
   document.body.classList.toggle("dark-theme");
 });
-document.querySelectorAll(".card-bookmark").forEach((boomark) => {
+document.querySelectorAll(".card-bookmark")?.forEach((boomark) => {
   boomark.addEventListener("click", (e) => {
     e.stopPropagation();
     e.target.classList.toggle("is-active");
