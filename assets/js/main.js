326f78ec94942ef11140d73abaf8f7bafec32847
khilda <joinyou42995@naver.com>
2023-04-06 17:00:15 +0900

    소스코드 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-// main page
+/**
+ * Common Func
+ */
 document.querySelector("#gnbOpen")?.addEventListener("click", () => {
   document.querySelector(".header-menu").classList.add("is-show");
   document.body.setAttribute("style", "overflow: hidden");
@@ -7,10 +9,12 @@ document.querySelector("#gnbClose")?.addEventListener("click", () => {
   document.querySelector(".header-menu").classList.remove("is-show");
   document.body.removeAttribute("style");
 });
+// theme
 document.querySelector("#toggleTheme")?.addEventListener("click", (e) => {
   e.target.classList.toggle("toggle");
   document.body.classList.toggle("dark-theme");
 });
+// card Bookmark
 document.querySelectorAll(".card-bookmark")?.forEach((boomark) => {
   boomark.addEventListener("click", (e) => {
     e.stopPropagation();
