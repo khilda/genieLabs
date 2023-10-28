aa9c6607aa478197e2596db91b93a678c8d5ba4d
joinyou <joinyou42995@naver.com>
2023-10-28 15:29:06 +0900

    genielabs mobile/tablet gnb 수정

--- labs_2.0/resources/js/menu_renew.js
+++ labs_2.0/resources/js/menu_renew.js
@@ -1,33 +1,63 @@
 // 메인메뉴
 $(document).ready(function () {
+  const $header = $("header");
   const $1dep = $(".header_nav>li>a");
   const $2dep = $(".header_nav>li>ul");
   // 초기화
   $(".header_nav").on("mouseleave", function (e) {
     $2dep.removeClass("on fix").addClass("off");
   });
-  // hover event
-  $1dep.on("mouseenter", function (e) {
-    $2dep.removeClass("on fix").addClass("off");
-    $(this).next().removeClass("off").addClass("on fix");
+  // header event
+  $1dep.on("click mouseenter", function (e) {
+    const $header = $("header");
+    const $subMenu = $(this).next();
+    const $siblings = $(this).parent("li").siblings("li").children("ul");
+
+    // tablet&mobile일때 mouseEnter 이벤트 해제
+    if (e.type === "mouseenter" && $header.hasClass("is-open")) return;
+
+    if ($header.hasClass("is-open")) {
+      // click
+      $siblings.removeClass("on fix").addClass("off");
+      if ($subMenu.hasClass("off")) {
+        $subMenu.removeClass("off").addClass("on fix");
+      } else {
+        $subMenu.removeClass("on fix").addClass("off");
+      }
+    } else {
+      // mouseenter
+      $2dep.removeClass("on fix").addClass("off");
+      $subMenu.removeClass("off").addClass("on fix");
+    }
+  });
+
+  // mobile menu
+  $(".openCloseMenu").on("click", function (e) {
+    if ($header.hasClass("is-open")) {
+      $header.removeClass("is-open");
+      $("body").removeAttr("style");
+    } else {
+      $header.addClass("is-open");
+      $("body").attr("style", "overflow:hidden");
+    }
   });
 });
 
 /**
- * Scroll Event
+ * window Event
  */
-$(window).on("scroll", (evt) => {
-  scrollHeader(evt);
+$(window).on({
+  scroll: function (e) {
+    let scrollY = $(this).scrollTop();
+    if (scrollY > 600) {
+      $("header").addClass("is-scroll");
+    } else {
+      $("header").removeClass("is-scroll");
+    }
+  },
+  resize: function (e) {
+    if (window.innerWidth >= 1440) {
+      $("header").removeClass("is-open");
+    }
+  },
 });
-
-/**
- * Header Scroll Toggle Class
- */
-function scrollHeader(evt) {
-  let scrollY = $(this).scrollTop();
-  if (scrollY > 600) {
-    $("header").addClass("is-scroll");
-  } else {
-    $("header").removeClass("is-scroll");
-  }
-}
